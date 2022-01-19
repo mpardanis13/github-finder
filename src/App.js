53786b27b54705a8697ad94a50d5ebd08187129c
mpardanis13 <mpardanis13@gmail.com>
2022-01-19 16:32:15 +0200

    second commit added search and alert

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import Alert from './components/layout/Alert'
 import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
+import { GithubProvider } from './context/github/GithubContext'
+import { AlertProvider } from './context/alert/AlertContext'
 
 function App() {
 	return (
-		<Router>
-			<div className='flex flex-col justify-between h-screen'>
-				<Navbar />
+		<GithubProvider>
+			<AlertProvider>
+				<Router>
+					<div className='flex flex-col justify-between h-screen'>
+						<Navbar />
 
-				<main className='container mx-auto px-3 pb-12'>
-					<Routes>
-						<Route path='/' element={<Home />}></Route>
-						<Route path='/about' element={<About />}></Route>
-						<Route path='/notfound' element={<NotFound />}></Route>
-						<Route path='/*' element={<NotFound />}></Route>
-					</Routes>
-				</main>
+						<main className='container mx-auto px-3 pb-12'>
+							<Alert />
+							<Routes>
+								<Route path='/' element={<Home />}></Route>
+								<Route path='/about' element={<About />}></Route>
+								<Route path='/notfound' element={<NotFound />}></Route>
+								<Route path='/*' element={<NotFound />}></Route>
+							</Routes>
+						</main>
 
-				<Footer />
-			</div>
-		</Router>
+						<Footer />
+					</div>
+				</Router>
+			</AlertProvider>
+		</GithubProvider>
 	)
 }
 
---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,59 @@
+import { createContext, useReducer } from 'react'
+import githubReducer from './GithubReducer'
+
+const GithubContext = createContext()
+
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+export const GithubProvider = ({ children }) => {
+	const initialState = {
+		users: [],
+		loading: false,
+	}
+
+	const [state, dispatch] = useReducer(githubReducer, initialState)
+
+	// Get Search Results
+	const searchUsers = async (text) => {
+		setLoading()
+
+		const params = new URLSearchParams({
+			q: text,
+		})
+
+		const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+			headers: {
+				Authorization: `token ${GITHUB_TOKEN}`,
+			},
+		})
+
+		const { items } = await res.json()
+
+		dispatch({
+			type: 'GET_USERS',
+			payload: items,
+		})
+	}
+
+	// Set Loading
+	const setLoading = () => dispatch({ type: 'SET_LOADING' })
+
+	//Clear Search Results
+	const clearUsers = () => dispatch({ type: 'CLEAR_USERS' })
+
+	return (
+		<GithubContext.Provider
+			value={{
+				users: state.users,
+				loading: state.loading,
+				searchUsers,
+				clearUsers,
+			}}
+		>
+			{children}
+		</GithubContext.Provider>
+	)
+}
+
+export default GithubContext
