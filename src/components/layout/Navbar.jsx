6faf5b309c29797f157800a5c939df6575cb66b4
mpardanis13 <mpardanis13@gmail.com>
2022-01-20 10:17:46 +0200

    final commit

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,6 +2,7 @@ import { FaGithub } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
+//
 function Navbar({ title }) {
 	return (
 		<nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
